7264cbef374e1256fc3a2fc4b28445572dc29211
Erik Laco <erik.laco@seenode.com>
2024-05-20 21:25:28 +0200

    Init commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Welcome to seenode",
+        })
+    })
+
+    router.Run("0.0.0.0:80")
+}
